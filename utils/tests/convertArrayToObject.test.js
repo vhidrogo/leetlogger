4c67a35fbd8d0750c3e6f21559a83a28961fefe3
Victor Hidrogo <victor@nav.it>
2025-04-27 22:34:25 -0700

    feat: Implement util to convert array to object

---
+++ utils/tests/convertArrayToObject.test.js
@@ -0,0 +1,18 @@
+const { convertArrayToObject } = require('../convertArrayToObject');
+
+describe('convertArrayToObject', () => {
+    it('converts keys and values into an object', () => {
+        const keys = ['id', 'name', 'difficulty'];
+        const values = [1, 'Two Sum', 'Easy'];
+        const expected = { id: 1, name: 'Two Sum', difficulty: 'Easy' };
+    
+        expect(convertArrayToObject(keys, values)).toEqual(expected);
+    });
+
+    it('throws an error when keys and values length differ', () => {
+        const keys = ['id', 'name'];
+        const values = [1, 'Two Sum', 'Extra'];
+    
+        expect(() => convertArrayToObject(keys, values)).toThrow('Keys and values arrays must be of the same length.');
+    });
+});

5558eb210bb311020b4779bcd6c0b525b9bc8902
Victor Hidrogo <vhidrogo@uci.edu>
2025-04-30 13:49:28 -0700

    refactor: Specify columns to exclude instead of include when filtering

--- tests/utils/filter2DArrayColumns.test.js
+++ tests/utils/filter2DArrayColumns.test.js
@@ -1,20 +1,20 @@
 const { filter2DArrayColumns } = require('../../src/utils/filter2DArrayColumns');
 
 describe('filter2DArrayColumns', () => {
-    it('filters 2D array columns based on filterHeaders', () => {
+    it('filters 2D array columns based on excludeColumns', () => {
         const data = [
             ['id', 'name', 'difficulty', 'link'],
             [1, 'Two Sum', 'Easy', 'leetcode.com'],
             [1, 'Three Sum', 'Medium', 'leetcode.com'],
         ];
-        const filterHeaders = ['id', 'name'];
+        const excludeColumns = ['difficulty', 'link'];
         const expected = [
             ['id', 'name'],
             [1, 'Two Sum'],
             [1, 'Three Sum'],
         ]
 
-        const result = filter2DArrayColumns(data, filterHeaders);
+        const result = filter2DArrayColumns(data, excludeColumns);
 
         expect(result).toEqual(expected);
     });

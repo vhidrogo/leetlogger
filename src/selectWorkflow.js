c0b3242da88854bb474345a1ab895a3e8f54c44d
Victor Hidrogo <vhidrogo@uci.edu>
2025-05-20 12:03:24 -0700

    feat: Implement select problem workflow

---
+++ src/selectWorkflow.js
@@ -0,0 +1,41 @@
+const { generateProblemSelectionList } = require("./dataModelUtils/generateProblemSelectionList");
+const { getInputsFromSheetUI } = require("./sheetUtils/getInputsFromSheetUI");
+const { setInputsOnSheetUI } = require("./sheetUtils/setInputsOnSheetUI");
+const { isAttemptInProgress } = require("./workflowUtils");
+
+/**
+ * Handles the logic when the "Select" button is clicked in the LeetLogger UI.
+ *
+ * - Checks if an attempt is already in progress.  
+ * - If not, selects the next problem from the generated problem list.  
+ * - Displays the selected problem's attributes on the UI.
+ */
+function onSelectClick() {
+    if (isAttemptInProgress) {
+        SpreadsheetApp.getUi().alert('Attempt currently in progress!');
+        return;
+    }
+
+    const problems = generateProblemSelectionList();
+    const selectedProblem = problems[0];
+    displayCurrentProblem(selectedProblem);
+}
+
+/**
+ * Updates the UI with the selected problem's attributes.
+ *
+ * Retrieves the input fields map from the UI, maps problem attributes to the corresponding keys,  
+ * and writes the updated values back to the UI.
+ *
+ * @param {Object} problemAttributes - An object containing attribute key-value pairs for the selected problem.
+ */
+function displayCurrentProblem(problemAttributes) {
+    const inputsMap = getInputsFromSheetUI('ControlPanel_CurrentProblemInputs');
+    for (const key of inputsMap.keys()) {
+        if (problemAttributes.hasOwnProperty(key)) {
+            inputsMap.set(key, problemAttributes[key]);
+        }
+    }
+
+    setInputsOnSheetUI('ControlPanel_CurrentProblemInputs', inputsMap);
+}

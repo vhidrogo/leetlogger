6889952c29b4127a1b2b77a92265ac399bb7a7c2
Victor Hidrogo <vhidrogo@uci.edu>
2025-05-20 20:21:30 -0700

    docs: Add JSDoc

--- src/optimalWorkflow.js
+++ src/optimalWorkflow.js
@@ -2,6 +2,14 @@ const { getInputsFromSheetUI } = require("./sheetUtils/getInputsFromSheetUI");
 const { setInputsOnSheetUI } = require("./sheetUtils/setInputsOnSheetUI");
 const { isAttemptDone } = require("./workflowUtils");
 
+/**
+ * Marks key attempt evaluation fields as 'Yes' in the UI if the attempt is complete.
+ * 
+ * If no attempt is marked as done, displays an alert and exits.
+ * Otherwise, sets relevant dropdown fields in the control panel inputs range to 'Yes'.
+ *
+ * @returns {void}
+ */
 function onOptimalClick() {
     if (!isAttemptDone()) {
         SpreadsheetApp.getUi().alert('Finish an attempt first before marking optimal.');

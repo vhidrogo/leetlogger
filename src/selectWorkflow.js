20b7c3ba8c96ab8717b68021504a60bdb160be0f
Victor Hidrogo <vhidrogo@uci.edu>
2025-05-29 15:33:43 -0700

    refactor: Call helper to select new problem

--- src/selectWorkflow.js
+++ src/selectWorkflow.js
@@ -1,14 +1,14 @@
-const { generateProblemSelectionList } = require("./dataModelUtils/generateProblemSelectionList");
-const { isAttemptInProgress, displayProblemListProgress, updateCurrentProblem, updateSelectionMetrics } = require("./workflowUtils");
-const { setNamedRangeValue } = require("./sheetUtils/setNamedRangeValue");
-const { NAMED_RANGES } = require("./constants");
+const { isAttemptInProgress, restartProblemSelection } = require("./workflowUtils");
 
 /**
  * Handles the logic when the "Select" button is clicked in the LeetLogger UI.
  *
  * - Checks if an attempt is already in progress.  
- * - If not, selects the next problem from the generated problem list.  
- * - Displays the selected problem's attributes on the UI.
+ * - If not, regenerates the problem selection list, updates selection metrics, 
+ *   clears the skip count display, and displays the next problem.
+ * 
+ * This initializes a fresh problem selection session and should be used
+ * when starting a new selection workflow.
  */
 function onSelectClick() {
     if (isAttemptInProgress()) {
@@ -16,10 +16,5 @@ function onSelectClick() {
         return;
     }
 
-    const problems = generateProblemSelectionList();
-    const problemIndex = 0;
-    
-    updateSelectionMetrics(problems);
-    setNamedRangeValue(NAMED_RANGES.ControlPanel.SKIP_COUNT, '');
-    updateCurrentProblem(problems[problemIndex]);
+    restartProblemSelection();
 }

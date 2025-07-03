cfa988134ec091b3690a962680d849dff7d0f734
Victor Hidrogo <vhidrogo@uci.edu>
2025-07-03 08:29:28 -0700

    feat: Prompt for confimation when all optimal inputs false

--- src/logWorkflow.js
+++ src/logWorkflow.js
@@ -5,9 +5,30 @@ const { getSheetByName } = require("./sheetUtils/getSheetByName");
 const { resetAttemptInputs, restartProblemSelection, clearCurrentProblem } = require("./workflowUtils");
 
 function onLogClick() {
+    const optimalInputs = getInputValues(NAMED_RANGES.AttemptInProgress.OPTIMAL_INPUTS);
+    if (optimalInputs.every(value => value === false)) {
+        const ui = SpreadsheetApp.getUi();
+        const response = ui.alert(
+            'Confirmation',
+            'All optimal inputs are false, are you sure you want to continue?',
+            ui.ButtonSet.YES_NO
+        );
+        if (response === ui.Button.NO) return;
+    }
+
     logAttempt();
+
+    const attemptInitiator = getNamedRangeValue(NAMED_RANGES.AttemptInProgress.INITIATOR);
+    if (attemptInitiator === PROBLEM_SELECTORS.GROUP_SELECTION) {
+        restartProblemSelection();
+    }
+    else if (attemptInitiator === PROBLEM_SELECTORS.SINGLE_SELECTION) {
+        clearCurrentProblem(PROBLEM_SELECTORS.SINGLE_SELECTION);
+    }
+
     resetAttemptInputs();
-    restartProblemSelection();
+    getSheetByName(attemptInitiator).activate();
+    getSheetByName(SHEET_NAMES.ATTEMPT_IN_PROGRESS).hideSheet();
 }
 
 function logAttempt() {
@@ -21,16 +42,4 @@ function logAttempt() {
     const inputValues = getInputValues(NAMED_RANGES.AttemptInProgress.INPUTS, requiredFields);
 
     appendRowToSheet(SHEET_NAMES.ATTEMPTS, inputValues);
-
-    const attemptInitiator = getNamedRangeValue(NAMED_RANGES.AttemptInProgress.INITIATOR);
-    try {
-        SpreadsheetApp.getActiveSpreadsheet().getSheetByName(attemptInitiator).activate();
-    } catch (e) {
-        return;
-    }
-    if (attemptInitiator == PROBLEM_SELECTORS.SINGLE_SELECTION) {
-        clearCurrentProblem(PROBLEM_SELECTORS.SINGLE_SELECTION);
-    }
-
-    getSheetByName(SHEET_NAMES.ATTEMPT_IN_PROGRESS).hideSheet();
 }
